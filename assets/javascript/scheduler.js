5d25bd3a761c11bfcc3a32361bce5f40795f05b7
Melanie Bott <melaniebott@gmail.com>
2020-04-13 21:43:43 -0600

    found error stopping firebase from being read and corrected. updated firebase rules as true

--- assets/javascript/scheduler.js
+++ assets/javascript/scheduler.js
@@ -3,108 +3,92 @@
 //provide up-to-date information about various trains
 //arrival times
 //how many minutes remain until they arrive at the station
+$(document).ready(function() {
 
-// Initialize Firebase
+    // Initialize Firebase
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCCgdsUpFhc4Vz9xlikRAYm8vb-DsVgzgk",
-    authDomain: "week7-scheduler.firebaseapp.com",
-    databaseURL: "https://week7-scheduler.firebaseio.com",
-    projectId: "week7-scheduler",
-    storageBucket: "week7-scheduler.appspot.com",
-    messagingSenderId: "25269581982",
-    appId: "1:25269581982:web:a0ab851caf9ba95c55638f",
-    measurementId: "G-07YYPMCVGY"
-};
-/* // Your web app's Firebase configuration */
-// var firebaseConfig = {
-//     apiKey: "AIzaSyAo69pU-EG7ldTuJrs4IpM3vvoi_fALyFA",
-//     authDomain: "trainscheduler-e858c.firebaseapp.com",
-//     databaseURL: "https://trainscheduler-e858c.firebaseio.com",
-//     projectId: "trainscheduler-e858c",
-//     storageBucket: "trainscheduler-e858c.appspot.com",
-//     messagingSenderId: "20057657526",
-//     appId: "1:20057657526:web:0b0a6ccc285a24f73e5617",
-//     measurementId: "G-9BQBJ2GL5P"
-// };
-/* // Initialize Firebase */
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+    var firebaseConfig = {
+        apiKey: "AIzaSyCCgdsUpFhc4Vz9xlikRAYm8vb-DsVgzgk",
+        authDomain: "week7-scheduler.firebaseapp.com",
+        databaseURL: "https://week7-scheduler.firebaseio.com",
+        projectId: "week7-scheduler",
+        storageBucket: "week7-scheduler.appspot.com",
+        messagingSenderId: "25269581982",
+        appId: "1:25269581982:web:a0ab851caf9ba95c55638f",
+        measurementId: "G-07YYPMCVGY"
+    };
+    /* // Original Firebase configuration. Created a new one, above because it isn't reading*/
+    // var firebaseConfig = {
+    //     apiKey: "AIzaSyAo69pU-EG7ldTuJrs4IpM3vvoi_fALyFA",
+    //     authDomain: "trainscheduler-e858c.firebaseapp.com",
+    //     databaseURL: "https://trainscheduler-e858c.firebaseio.com",
+    //     projectId: "trainscheduler-e858c",
+    //     storageBucket: "trainscheduler-e858c.appspot.com",
+    //     messagingSenderId: "20057657526",
+    //     appId: "1:20057657526:web:0b0a6ccc285a24f73e5617",
+    //     measurementId: "G-9BQBJ2GL5P"
+    // };
+    /* // Initialize Firebase */
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
 
-// Variables
-//===============================================================
-//get a reference to the database service
-var database = firebase.database();
+    // Variables
+    //===============================================================
+    //get a reference to the database service
+    var database = firebase.database();
 
-var train;
-var destination;
-var frequency;
-var newTrain;
-//snapshot variables
-var newDestination;
-var newFrequency;
-var minutesRemaining;
-var trainArrival;
-var addTrain;
-var refKey;
-var key;
 
-// add train form submitted
-$("#formGroup").on("formSubmit", function(event) {
+    // add train form submitted
+    $("#formSubmit").on("click", function(event) {
         event.preventDefault();
-        // get values and push to database
-        getValues("#train-name", "#train-destination", "#frequency");
-        database.ref().push(addTrain);
-        // also set in firebase
-        $("train-name").val().trim();
-        $("train-destination").val().trim();
-        $("frequency").val().trim();
-    })
-    //create train object
-addTrain = {
-    trainName: trainName,
-    destination: destination,
-    frequency: frequency
-}
+        console.log("Why ME?")
+            // get values from html
+        var train = $("#train-name").val().trim();
+        var destination = $("#train-destination").val().trim();
+        var frequency = $("#frequency").val().trim();
+        var firstTrainTime = $("#train-time").val().trim();
 
-function convertTime(snapshot) {
-    newTrain = snapshot.val().train;
-    newDestination = snapshot.val().destination;
-    newFrequency = snapshot.val().frequency;
+        //clear user input after submission
+        $("#train-name").val("");
+        $("#train-destination").val("");
+        $("#frequency").val("");
+        $("#train-time").val("");
 
-    //database finds arrival time and minutes away
-    var timeConverted = moment(newTrainTime, "hh:mm").subtract(1, "years");
-    var timeDifference = moment().diff(moment(trainTimeConverted), "minutes");
-    var timeLeftOver = timeDifference % newFrequency;
-    minutesRemaining = newFrequency - timeLeftOver;
-    trainArrival = moment().add(minutesRemaining, "minutes").format("hh:mm");
-}
-// train
-function createTrain() {
-    var rowElement = $("<div>").attr("data-key", refKey).addClass("row-train");
-    var nameCell = $("<div>").addClass("trainItem").text(newTrain);
-    var destination = $("<span>").text()
-}
+        // move variables into the firebase
+        database.ref().push({
+            trainName: train,
+            destination: destination,
+            frequency: frequency,
+            firstTrainTime: firstTrainTime
+        });
+        console.log(train + destination + frequency + firstTrainTime);
+    });
+
+    // define function
 
 
-//object added to database
 
-database.ref().on("line_added", function(snapshot) {
-    refKey = snapshot.key;
-    convertTime(snapshot);
-    createTrain();
-})
+    //call functions if a child is added to database
 
-database.ref().on("line_changed", function(snapshot) {
-    convertTime(snapshot);
-    $(".train-row").each(function() {
-        if ($(this).attr("data-key") === key) {
-            //update html with new form values
-            $(this).children(".train-cell").text(newTrain);
-            $(this).children(".destination-container").children(".destination-cell").text(newDestination);
-            $(this).children(".frequency-cell").text(newFrequency);
-            $(this).children(".arrival-cell").attr("data-time", newTrainTime).text(trainArrival);
-            $(this).children(".minutes-cell").text(minutesRemaining);
-        }
-    })
-})
+    database.ref().on("child_added", function(snapshot) {
+        console.log("made it!");
+        // var refKey = snapshot.key;
+        var train = snapshot.val().trainName;
+        var destination = snapshot.val().destination;
+        var frequency = snapshot.val().frequency;
+        var firstTrainTime = snapshot.val().firstTrainTime;
+
+        //database finds arrival time and minutes away
+        var timeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+        var timeDifference = moment().diff(moment(timeConverted), "minutes");
+        var timeLeftOver = timeDifference % frequency;
+        var minutesAway = frequency - timeLeftOver;
+        var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm");
+
+        $("#tableContent").prepend("<tr><td>" + train + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" +
+            nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+    }, function(err) {
+        console.log("Firebase Sucks!");
+    });
+
+});
